d2a700f51327efada2bb1c733de8994743d6bbf4
Git-Kentaro <83595516+Git-Kentaro@users.noreply.github.com>
2021-05-12 22:45:58 +0900

    Add files via upload

---
+++ java_introduction_book/comment/Zenhan.java
@@ -0,0 +1,12 @@
+package comment;
+
+//chapter6で使用するクラス
+public class Zenhan {
+	public static void doWarusa() {
+		System.out.println("きなこでござる。食えませんがの。");
+	}
+
+	public static void doTogame() {
+		System.out.println("この老いぼれの目は誤魔化せませんぞ。");
+	}
+}
