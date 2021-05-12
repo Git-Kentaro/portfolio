86b0c95865aa5f355de652e2e62620f70a61b33b
Git-Kentaro <83595516+Git-Kentaro@users.noreply.github.com>
2021-05-12 22:37:19 +0900

    Add files via upload

---
+++ java_introduction_book/chapter1/Code1_5.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class Code1_5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 4;
+//		書き換え防止
+		final double PI = 3.14; /* 定数はfinal をつけ、大文字で名前をつける */
+		System.out.println("半径" + x + "の円の面積は、" + x * x * PI + "です。");
+
+	}
+
+}
