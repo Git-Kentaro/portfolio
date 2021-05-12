fa7832fd0cc866f75614ba4c3d156e5d6a55f9d2
Git-Kentaro <83595516+Git-Kentaro@users.noreply.github.com>
2021-05-12 22:38:05 +0900

    Add files via upload

---
+++ java_introduction_book/chapter2/Code2_2.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class Code2_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("私の好きな記号は、二重引用符 (\") です");
+//		エスケープシークエンスを使って、意味を変える
+//		\ + 表したいもの　で表現する
+	}
+
+}
---
+++ java_introduction_book/chapter2/Code2_7.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class Code2_7 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int age = (int)3.2;  /* 3.2はdouble型だが、強制的にint型に型変換 */
+		System.out.println(age);
+	}
+
+}
