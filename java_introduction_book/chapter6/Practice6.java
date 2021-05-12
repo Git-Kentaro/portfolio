f348bfc782495abc9d79925ee6b86046e52444d5
Git-Kentaro <83595516+Git-Kentaro@users.noreply.github.com>
2021-05-12 22:42:09 +0900

    Add files via upload

---
+++ java_introduction_book/chapter6/Practice6.java
@@ -0,0 +1,15 @@
+package chapter6;
+
+import comment.Zenhan;
+
+public class Practice6 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO 自動生成されたメソッド・スタブ
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+
+}
